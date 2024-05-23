8d4d39655d2bfd32f2262ce32127a80a2bbef40d
centSadili <sadiliva@students.nu-moa.edu.ph>
2024-05-23 23:12:50 +0800

    added popup msg for sign in and sign up

--- script.js
+++ script.js
@@ -48,3 +48,47 @@ $(document).ready(function() {
         source: cities
     });
 });
+
+
+// for sign in and log in pop up
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal elements
+    var loginModal = document.getElementById("loginModal");
+    var signupModal = document.getElementById("signupModal");
+
+    // Get the button that opens the modal
+    var loginBtn = document.getElementById("loginBtn");
+    var signupBtn = document.getElementById("signupBtn");
+
+    // Get the <span> elements that close the modal
+    var closeLogin = document.getElementById("closeLogin");
+    var closeSignup = document.getElementById("closeSignup");
+
+    // When the user clicks the button, open the modal 
+    loginBtn.onclick = function() {
+        loginModal.style.display = "block";
+    }
+
+    signupBtn.onclick = function() {
+        signupModal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    closeLogin.onclick = function() {
+        loginModal.style.display = "none";
+    }
+
+    closeSignup.onclick = function() {
+        signupModal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == loginModal) {
+            loginModal.style.display = "none";
+        }
+        if (event.target == signupModal) {
+            signupModal.style.display = "none";
+        }
+    }
+});
