d1c1575359b6200a66154170eb13031a88768bb0
Spencer Daniel Madia <madiasm@students.nu-moa.edu.ph>
2024-06-01 16:29:26 +0800

    Modified and fixed JS Dependencies and Database

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 // Define routes
 router.get("/", (req, res) => {
     res.render("login");
